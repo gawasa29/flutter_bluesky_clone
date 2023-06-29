7bd944ce0d94d18bc20fb39f17a271a3c608cce8
Me is gawasa <61944230+gawasa29@users.noreply.github.com>
2023-06-29 13:39:53 +0900

    refactor: home_screen

--- lib/features/post/view/home_screen.dart
+++ lib/features/post/view/home_screen.dart
@@ -105,44 +105,27 @@ class EachPost extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final colors = theme.colorScheme;
     final typography = theme.textTheme;
-    return Padding(
-        padding: const EdgeInsets.only(top: 5),
-        child: Column(
+    return Column(
+      children: [
+        const SizedBox(height: 10),
+        Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 16, right: 8),
+            const SizedBox(width: 10),
+            const CircleAvatar(
+              radius: 25,
+              backgroundImage: NetworkImage(
+                  'https://images.unsplash.com/photo-1491349174775-aaafddd81942?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Row(
                     children: [
-                      const CircleAvatar(
-                        radius: 25,
-                        backgroundImage: NetworkImage(
-                            'https://images.unsplash.com/photo-1491349174775-aaafddd81942?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'),
-                      ),
-                      const SizedBox(
-                        width: 8,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'UserName',
-                            style: typography.titleMedium!.copyWith(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            '@HandleName',
-                            style: typography.bodySmall!
-                                .copyWith(color: colors.onSecondary),
-                          ),
-                        ],
-                      ),
+                      const UserInfo(),
                       Expanded(child: Container()),
                       ElevatedButton(
                         onPressed: () {},
@@ -155,140 +138,156 @@ class EachPost extends StatelessWidget {
                       ),
                     ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 50, left: 50),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8),
-                          child: Text(
-                            'Posts will appear here.',
-                            style: typography.bodyLarge!.copyWith(),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              InkWell(
-                                onTap: () {
-                                  print('Tap Reply');
-                                },
-                                child: const Row(
-                                  children: [
-                                    Icon(Icons.chat_bubble_outline),
-                                    Text('0'),
-                                  ],
-                                ),
-                              ),
-                              InkWell(
-                                onTap: () {
-                                  print('Tap Retweet');
-                                },
-                                child: const Row(
-                                  children: [
-                                    Icon(Icons.repeat),
-                                    Text('0'),
-                                  ],
-                                ),
-                              ),
-                              InkWell(
-                                onTap: () {
-                                  print('Tap Like');
-                                },
-                                child: const Row(
-                                  children: [
-                                    Icon(Icons.favorite_border),
-                                    Text('0'),
-                                  ],
-                                ),
-                              ),
-                              PopupMenuButton<int>(
-                                icon: const Icon(Icons.more_horiz),
-                                position: PopupMenuPosition.under,
-                                onSelected: print,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                itemBuilder: (context) {
-                                  return [
-                                    const PopupMenuItem(
-                                      value: 0,
-                                      child: Row(
-                                        children: [
-                                          Icon(Icons.g_translate),
-                                          SizedBox(width: 5),
-                                          Text('Translate...'),
-                                        ],
-                                      ),
-                                    ),
-                                    const PopupMenuItem(
-                                      value: 1,
-                                      child: Row(
-                                        children: [
-                                          Icon(Icons.copy_rounded),
-                                          SizedBox(width: 5),
-                                          Text('Copy post text'),
-                                        ],
-                                      ),
-                                    ),
-                                    const PopupMenuItem(
-                                      value: 2,
-                                      child: Row(
-                                        children: [
-                                          Icon(Icons.reply),
-                                          SizedBox(width: 5),
-                                          Text('Share...'),
-                                        ],
-                                      ),
-                                    ),
-                                    const PopupMenuDivider(),
-                                    const PopupMenuItem(
-                                      value: 3,
-                                      child: Row(
-                                        children: [
-                                          Icon(Icons.speaker_notes_off),
-                                          SizedBox(width: 5),
-                                          Text('Mute thread'),
-                                        ],
-                                      ),
-                                    ),
-                                    const PopupMenuDivider(),
-                                    const PopupMenuItem(
-                                      value: 4,
-                                      child: Row(
-                                        children: [
-                                          Icon(Icons.delete),
-                                          SizedBox(width: 5),
-                                          Text('Delete post'),
-                                        ],
-                                      ),
-                                    ),
-                                  ];
-                                },
-                              )
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                  Text(
+                    'Posts will appear here.',
+                    style: typography.bodyLarge!.copyWith(),
                   ),
+                  const ActionIcons(),
                 ],
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.only(right: 28, left: 28),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              ),
-            ),
-            const SizedBox(
-              height: 0,
-            ),
-            const Divider(thickness: 0.5),
           ],
-        ));
+        ),
+        const Divider(thickness: 0.5),
+      ],
+    );
+  }
+}
+
+class UserInfo extends StatelessWidget {
+  const UserInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final typography = theme.textTheme;
+    final colors = theme.colorScheme;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'UserName',
+          style: typography.titleMedium!.copyWith(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          '@HandleName',
+          style: typography.bodySmall!.copyWith(
+            color: colors.onSecondary,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ActionIcons extends StatelessWidget {
+  const ActionIcons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        InkWell(
+          onTap: () {
+            print('Tap Reply');
+          },
+          child: const Row(
+            children: [
+              Icon(Icons.chat_bubble_outline),
+              Text('0'),
+            ],
+          ),
+        ),
+        InkWell(
+          onTap: () {
+            print('Tap Retweet');
+          },
+          child: const Row(
+            children: [
+              Icon(Icons.repeat),
+              Text('0'),
+            ],
+          ),
+        ),
+        InkWell(
+          onTap: () {
+            print('Tap Like');
+          },
+          child: const Row(
+            children: [
+              Icon(Icons.favorite_border),
+              Text('0'),
+            ],
+          ),
+        ),
+        PopupMenuButton<int>(
+          icon: const Icon(Icons.more_horiz),
+          position: PopupMenuPosition.under,
+          onSelected: print,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          itemBuilder: (context) {
+            return [
+              const PopupMenuItem(
+                value: 0,
+                child: Row(
+                  children: [
+                    Icon(Icons.g_translate),
+                    SizedBox(width: 5),
+                    Text('Translate...'),
+                  ],
+                ),
+              ),
+              const PopupMenuItem(
+                value: 1,
+                child: Row(
+                  children: [
+                    Icon(Icons.copy_rounded),
+                    SizedBox(width: 5),
+                    Text('Copy post text'),
+                  ],
+                ),
+              ),
+              const PopupMenuItem(
+                value: 2,
+                child: Row(
+                  children: [
+                    Icon(Icons.reply),
+                    SizedBox(width: 5),
+                    Text('Share...'),
+                  ],
+                ),
+              ),
+              const PopupMenuDivider(),
+              const PopupMenuItem(
+                value: 3,
+                child: Row(
+                  children: [
+                    Icon(Icons.speaker_notes_off),
+                    SizedBox(width: 5),
+                    Text('Mute thread'),
+                  ],
+                ),
+              ),
+              const PopupMenuDivider(),
+              const PopupMenuItem(
+                value: 4,
+                child: Row(
+                  children: [
+                    Icon(Icons.delete),
+                    SizedBox(width: 5),
+                    Text('Delete post'),
+                  ],
+                ),
+              ),
+            ];
+          },
+        ),
+      ],
+    );
   }
 }
